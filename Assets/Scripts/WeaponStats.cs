f3997bdaae681869dc328af96627f28b21df74ff
Karades <marcin.kon1998@gmail.comgit config --global user.name Karadesgit config --global user.email marcin.kon1998@gmail.com>
2020-06-11 14:23:30 +0200

    Weapon stat holder
    Added Weapon Stats script, which is responsible for holding stats like firing rate, ammo etc. Now works for Pistol only

---
+++ Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour
+{
+    public int gunAmmo;
+    public int maxGunAmmo = 30;
+    public float gunFiringSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunAmmo = maxGunAmmo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
