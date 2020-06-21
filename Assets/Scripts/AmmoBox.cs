06355681a676ae7be5d911ca7d30db2a5b37aee4
Karades <marcin.kon1998@gmail.comgit config --global user.name Karadesgit config --global user.email marcin.kon1998@gmail.com>
2020-06-21 12:16:28 +0200

    Box for refreshing ammo
    Added rotating box on map. When player hits it, box is destroyed and full ammo is refreshed

---
+++ Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEditor;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    public WeaponStats weaponStats;
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponStats = GameObject.Find("WeaponStats").GetComponent<WeaponStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate(speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        if(other.tag == "Player")
+        {
+            weaponStats.refreshAmmo();
+            Destroy(gameObject); ;
+        }
+    }
+    void Rotate(float speed)
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
